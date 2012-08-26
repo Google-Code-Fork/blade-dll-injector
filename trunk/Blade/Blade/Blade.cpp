648e8cf4c6129d597f900c63fa05affed79a12e1
m.skrzetuski@gmail.com <m.skrzetuski@gmail.com@2d5af108-797f-caf9-f938-b6b83cf94a45>
2012-08-26 12:54:44 +0000

    Ejection just plain works.

--- trunk/Blade/Blade/Blade.cpp
+++ trunk/Blade/Blade/Blade.cpp
@@ -1,6 +1,8 @@
 #include "Injector.h"
 #include "Logger.h"
 
+using namespace System::Threading;
+
 int main() 
 {
 	Injector^ injector = gcnew Injector();
@@ -8,6 +10,10 @@ int main()
 	Logger::Instance->Log("Blade is starting.");
 
 	injector->Inject("iexplore", "Poison.dll");
+
+	Thread::Sleep(5000);
+
+	injector->Eject("iexplore", "Poison.dll");
 
 	Logger::Instance->Log("Blade is closing.");
 	return 0;
--- trunk/Blade/Blade/Injector.cpp
+++ trunk/Blade/Blade/Injector.cpp
@@ -29,76 +29,133 @@ String^ Injector::GetLastErrorMessage()
 	return Marshal:: PtrToStringUni(ptr);
 }
 
-bool Injector::Inject(String^ processName, String^ dllName) 
+bool Injector::Eject(String^ processName, String^ dllName)
 {
 	String^ dllPath = Environment::CurrentDirectory + "\\" + dllName;
 	array<Process^>^ processes = Process::GetProcessesByName(processName);
 
+	Int32 processId = 0;
 	if (processes->Length > 0)
 	{
-		Int32 processId = processes[0]->Id;
+		processId = processes[0]->Id;
 		Logger::Instance->Log("Process id of " +  processName + " is " + processId + ".");
-
-		System::Collections::IEnumerator^ module = processes[0]->Modules->GetEnumerator();
-		while (module->MoveNext())
-		{
-			Logger::Instance->Log("Loaded module " + dynamic_cast<ProcessModule^>(module->Current)->FileName  + ".");
-
-			if (dllPath == static_cast<ProcessModule^>(module->Current)->FileName)
-			{
-				Logger::Instance->Log("DLL " + dllPath + " already injected.");
-				return false;
-			}
-		} 
-
-		Logger::Instance->Log("Openning process.");
-		hProcess = OpenProcess(PROCESS_ALL_ACCESS, false, processId);
-
-		if (hProcess == NULL)
-		{
-			Logger::Instance->Log("Error while openning process with message '" + GetLastErrorMessage() + "'.");
-			return false;
-		}
-
-		Logger::Instance->Log("Allocating memory for " + dllPath + ".");
-		allocAddress = VirtualAllocEx(hProcess, 0, (dllPath->Length)*sizeof(wchar_t), MEM_COMMIT, PAGE_EXECUTE_READWRITE);
-
-		if (allocAddress == NULL)
-		{
-			Logger::Instance->Log("Error while allocating memory with message '" + GetLastErrorMessage() + "'.");
-			return false;
-		}
-
-		Logger::Instance->Log("Writing path to remote process.");
-		if (WriteProcessMemory(hProcess, (void*)(allocAddress), (void*)Marshal::StringToHGlobalUni(dllPath), (dllPath->Length)*sizeof(wchar_t), 0) == 0)
-		{
-			Logger::Instance->Log("Error while writing path to remote process with message '" + GetLastErrorMessage() + "'.");
-			return false;
-		}
-
-		Logger::Instance->Log("Loading DLL remotely.");
-		hRemoteThread = CreateRemoteThread(hProcess, 0, 0, (LPTHREAD_START_ROUTINE) GetProcAddress(GetModuleHandle(L"kernel32.dll"), "LoadLibraryW"), allocAddress, 0, 0);
-
-		if (hRemoteThread == NULL)
-		{
-			Logger::Instance->Log("Error while calling remote thread with message '" + GetLastErrorMessage() + "'.");
-			return false;
-		}
-
-		Logger::Instance->Log("Waiting.");
-		WaitForSingleObject(hRemoteThread, INFINITE);
-
-		Logger::Instance->Log("Freeing.");
-	    VirtualFreeEx(hProcess, allocAddress, (dllPath->Length)*sizeof(wchar_t), MEM_DECOMMIT);
-
-		Logger::Instance->Log("Closing.");
-		CloseHandle(hProcess);
 	}
 	else
 	{
 		Logger::Instance->Log("No processes named " +  processName + " found.");
 		return false;
 	}
+
+	ProcessModule^ processModule;
+	System::Collections::IEnumerator^ module = processes[0]->Modules->GetEnumerator();
+	while (module->MoveNext())
+	{
+		Logger::Instance->Log("Loaded module " + dynamic_cast<ProcessModule^>(module->Current)->FileName  + ".");
+
+		if (dllPath == static_cast<ProcessModule^>(module->Current)->FileName)
+		{
+			Logger::Instance->Log("DLL " + dllPath + " found. Will remove it.");
+			processModule = static_cast<ProcessModule^>(module->Current);
+		}
+	}
+
+	Logger::Instance->Log("Openning process.");
+	hProcess = OpenProcess(PROCESS_ALL_ACCESS, false, processId);
+		
+	if (hProcess == NULL)
+	{
+		Logger::Instance->Log("Error while openning process with message '" + GetLastErrorMessage() + "'.");
+		return false;
+	}
+
+	Logger::Instance->Log("Unloading DLL remotely.");
+	hRemoteThread = CreateRemoteThread(hProcess, 0, 0, (LPTHREAD_START_ROUTINE) GetProcAddress(GetModuleHandle(L"kernel32.dll"), "FreeLibrary"), (void*)processModule->BaseAddress, 0, 0);
+
+	if (hRemoteThread == NULL)
+	{
+		Logger::Instance->Log("Error while calling remote thread with message '" + GetLastErrorMessage() + "'.");
+		return false;
+	}
+
+	Logger::Instance->Log("Waiting.");
+	WaitForSingleObject(hRemoteThread, INFINITE);
+
+	Logger::Instance->Log("Closing.");
+	CloseHandle(hProcess);
+}
+
+bool Injector::Inject(String^ processName, String^ dllName) 
+{
+	String^ dllPath = Environment::CurrentDirectory + "\\" + dllName;
+	array<Process^>^ processes = Process::GetProcessesByName(processName);
+
+	Int32 processId = 0;
+	if (processes->Length > 0)
+	{
+		processId = processes[0]->Id;
+		Logger::Instance->Log("Process id of " +  processName + " is " + processId + ".");
+
+	}
+	else
+	{
+		Logger::Instance->Log("No processes named " +  processName + " found.");
+		return false;
+	}
+
+	System::Collections::IEnumerator^ module = processes[0]->Modules->GetEnumerator();
+	while (module->MoveNext())
+	{
+		Logger::Instance->Log("Loaded module " + static_cast<ProcessModule^>(module->Current)->FileName  + ".");
+
+		if (dllPath == static_cast<ProcessModule^>(module->Current)->FileName)
+		{
+			Logger::Instance->Log("DLL " + dllPath + " already injected.");
+			return false;
+		}
+	} 
+
+	Logger::Instance->Log("Openning process.");
+	hProcess = OpenProcess(PROCESS_ALL_ACCESS, false, processId);
+
+	if (hProcess == NULL)
+	{
+		Logger::Instance->Log("Error while openning process with message '" + GetLastErrorMessage() + "'.");
+		return false;
+	}
+
+	Logger::Instance->Log("Allocating memory for " + dllPath + ".");
+	allocAddress = VirtualAllocEx(hProcess, 0, (dllPath->Length)*sizeof(wchar_t), MEM_COMMIT, PAGE_EXECUTE_READWRITE);
+
+	if (allocAddress == NULL)
+	{
+		Logger::Instance->Log("Error while allocating memory with message '" + GetLastErrorMessage() + "'.");
+		return false;
+	}
+
+	Logger::Instance->Log("Writing path to remote process.");
+	if (WriteProcessMemory(hProcess, (void*)(allocAddress), (void*)Marshal::StringToHGlobalUni(dllPath), (dllPath->Length)*sizeof(wchar_t), 0) == 0)
+	{
+		Logger::Instance->Log("Error while writing path to remote process with message '" + GetLastErrorMessage() + "'.");
+		return false;
+	}
+
+	Logger::Instance->Log("Loading DLL remotely.");
+	hRemoteThread = CreateRemoteThread(hProcess, 0, 0, (LPTHREAD_START_ROUTINE) GetProcAddress(GetModuleHandle(L"kernel32.dll"), "LoadLibraryW"), allocAddress, 0, 0);
+
+	if (hRemoteThread == NULL)
+	{
+		Logger::Instance->Log("Error while calling remote thread with message '" + GetLastErrorMessage() + "'.");
+		return false;
+	}
+
+	Logger::Instance->Log("Waiting.");
+	WaitForSingleObject(hRemoteThread, INFINITE);
+
+	Logger::Instance->Log("Freeing.");
+	VirtualFreeEx(hProcess, allocAddress, (dllPath->Length)*sizeof(wchar_t), MEM_DECOMMIT);
+
+	Logger::Instance->Log("Closing.");
+	CloseHandle(hProcess);
 
 	return false;
 }
